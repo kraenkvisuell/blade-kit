7fc71793b62fedffaa61e16aeb9a3ddb915b1d50
Sebastian Hilger <hilger@kraenk.de>
2025-03-08 23:25:51 +0100

    wip

---
+++ export/helpers.php
@@ -0,0 +1,38 @@
+<?php
+
+function cdnImage($image, array $options = [])
+{
+    $output = $options['fileformat'] ?? 'webp';
+
+    $transformations = ['output=' . $output];
+
+    $crop = $options['crop'] ?? false;
+
+    if (isset($options['quality'])) {
+        $transformations[] = 'quality=' . $options['quality'];
+    }
+
+    if (@$options['focus']) {
+        $focusArr = array_map(fn($item) => intval($item) . 'p', array_filter(explode(' ', $options['focus'])));
+
+        if (count($focusArr) === 2) {
+            $transformations[] = 'focus=' . implode('x', $focusArr);
+        }
+    }
+
+    if ($crop && isset($options['fit']) && is_array($options['fit']) && count($options['fit']) === 2) {
+        $transformations[] = 'cover-max=' . implode('x', $options['fit']);
+    } elseif (isset($options['fit']) && is_array($options['fit']) && count($options['fit']) === 2) {
+        $transformations[] = 'contain-max=' . implode('x', $options['fit']);
+    } elseif (isset($options['width']) && !isset($options['height'])) {
+        $transformations[] = 'resize-max=' . $options['width'];
+    } elseif (!isset($options['width']) && isset($options['height'])) {
+        $transformations[] = 'resize-max=-x' . $options['height'];
+    } elseif (isset($options['width']) && isset($options['height'])) {
+        $transformations[] = 'resize-max=' . $options['width'] . 'x' . $options['height'];
+    }
+
+    return config('app.image_cdn')
+        . '/' . config('filesystems.disks.assets.root') . '/' . $image['path']
+        . '?twic=v1/' . implode('/', $transformations);
+}
